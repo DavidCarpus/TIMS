56d0ad577c9c1108da385343f0878ff9bd890176
David.Carpus <David.Carpus@csquaredsystems.com>
2018-11-10 14:12:01 -0500

    Initial framework with Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+import { MaterialModule } from './material.module';
+import { DepartmentComponent } from './Pages/department/department.component';
+import { HomeComponent } from './Pages/home/home.component';
+
+const appRoutes: Routes = [
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'department', component: DepartmentComponent },
+	{ path: 'home', component: HomeComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		MainBodyComponent,
+		DepartmentComponent,
+		HomeComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		RouterModule.forRoot(appRoutes),
+		MaterialModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
